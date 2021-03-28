800f5ae99e58182b6eb1f57e8be1523361d9d632
akinolajaye <akinolajaye@gmail.com>
2021-03-28 16:18:00 +0100

    added search function all functions working

--- User.java
+++ User.java
@@ -7,20 +7,23 @@ import java.util.Scanner;
 
 public class User {
     public static void main(String[] args) {
-        File f = new File("UserAccounts.txt");
-        User guy = new User("101", f);
-        guy.print();
-        guy.findAll(f);
+        User guy = new User("101", "UserAccounts.txt"); //creates new user
+
+
     }
 
     /* creates the attributes for the user */
    
 
     private String id, username, surname,houseNumber, postcode, city, role;
+    private String filename;
+    private String regex ="\\s*,\\s*";//defines a regex that removes commas and trailing white space
 
-    public User(String id, File file) {
+    public User(String id, String filename) {
 
-        List<String> userDataArray=findOne(id,file);
+        this.filename=filename;
+        
+        List<String> userDataArray=findOne(id,this.filename);
 
         this.id=userDataArray.get(0);
         username=userDataArray.get(1);
@@ -33,21 +36,11 @@ public class User {
         
     }
 
-   
 
-    public void print(){
-        System.out.println(this.id);
-        System.out.println(username);
-        System.out.println(surname);
-        System.out.println(houseNumber);
-        System.out.println(postcode);
-        System.out.println(city);
-        System.out.println(role);
 
-    }
+    public List<String> findOne(String id,String filename){
 
-    private List<String> findOne(String id, File file){
-
+        File file= new File(filename);
         Scanner fileReader; // scanner variable is created to read the file and return lines
         String dataStr; // string variable to hold unformatted string after line is read
          // array variable to hold splitted string
@@ -59,8 +52,8 @@ public class User {
             while (!found) {
                 
                 dataStr = fileReader.nextLine(); //reads/returns current line from the txt file
-                //dataArray = dataStr.split(",");//creates an array that splits the data by comma
-                dataArray = Arrays.asList(dataStr.split("\\s*,\\s*"));//creates an array that splits the data by comma using regex
+                
+                dataArray = Arrays.asList(dataStr.split(regex));//creates an array that splits the data by comma using regex
                 if (dataArray.get(0).equals(id)) {//checks if the id given as criteria is in the txt file
 
                     fileReader.close();
@@ -79,7 +72,9 @@ public class User {
     }
 
 
-    public List<List<String>> findAll(File file) {
+    public List<List<String>> viewBooks(String filename) {
+
+        File file= new File(filename);
 
         Scanner fileReader; // scanner variable is created to read the file and return lines
         String dataStr; // string variable to hold unformatted string after line is read
@@ -94,7 +89,7 @@ public class User {
             while (fileReader.hasNextLine()) {
                 
                 dataStr = fileReader.nextLine(); //reads/returns current line from the txt file
-                dataField = Arrays.asList(dataStr.split("\\s*,\\s*"));//creates an array that splits the data by comma using regex
+                dataField = Arrays.asList(dataStr.split(regex));//creates an array that splits the data by comma using regex
 
                 dataArray.add(j,dataField);//adds the data field list to data array to form 2d array
                 j++;
@@ -106,7 +101,7 @@ public class User {
             ;
         }
 
-        System.out.println(dataArray.toString());
+
 
         return dataArray;
 
@@ -117,6 +112,46 @@ public class User {
     }
 
 
+    public List<List<String>> searchBooks(String filename,List<String> searchField){
 
+        File file= new File(filename);//creates file variable
+        Scanner fileReader; // scanner variable is created to read the file and return lines
+        List<List<String>> resultArray=new ArrayList<>(); // 2darray variable to hold splitted data field strings
+        int i =0;// initializes i for the index when calling 'add' for the resultArray List
+        
+        try{
+            fileReader=new Scanner(file);//scans file
+
+            while(fileReader.hasNextLine()){//loop while the textfile has another line
+                String searchId =Arrays.asList(fileReader.nextLine().split(regex)).get(0);//gets and converts the text file line the retrieves the id
+                List<String> dataField=findOne(searchId, filename);//calls the find one function to get the single data row based on id
+                if (dataField.containsAll(searchField)){ //checks if the data being searched for is in the data field
+                    resultArray.add(i,dataField);//adds it to result array if true
+                    i++;
+
+
+
+
+                }
+            }
+
+        }catch(FileNotFoundException e){
+            ;
+        }
+
+        return resultArray;
+        
+        
+      
+
+        
+        
+
+
+    }
+
+
+
+    
 
 }
